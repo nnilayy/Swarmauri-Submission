68ac2618c7d1a74cf480749adaff6420ba62abcb
unknown <nnilayy.work@gmail.com>
2024-12-11 09:27:17 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React app's address
+    allow_origins=["*"],  # React app's address
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
